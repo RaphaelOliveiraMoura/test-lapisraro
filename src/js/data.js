952a4fb58c91f16456793f01774eeff997b78751
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-21 16:41:52 -0300

    start project structure and home page

---
+++ src/js/data.js
@@ -0,0 +1,46 @@
+const categories = [
+  {
+    title: 'Informática',
+    image: '',
+    services: [
+      {
+        name: 'Mouse Microsoft com 3 Botões Scroll - P5800061',
+        image: '',
+        price: 29.9
+      }
+    ]
+  },
+  {
+    title: 'Lazer',
+    image: '',
+    services: [
+      {
+        name: 'Mouse Microsoft com 3 Botões Scroll - P5800061',
+        image: '',
+        price: 29.9
+      }
+    ]
+  },
+  {
+    title: 'Moda',
+    image: '',
+    services: [
+      {
+        name: 'Mouse Microsoft com 3 Botões Scroll - P5800061',
+        image: '',
+        price: 29.9
+      }
+    ]
+  },
+  {
+    title: 'Moda',
+    image: '',
+    services: [
+      {
+        name: 'Mouse Microsoft com 3 Botões Scroll - P5800061',
+        image: '',
+        price: 29.9
+      }
+    ]
+  }
+];
